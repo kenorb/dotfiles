9096342625feada9f86aca46c262b7eec873cb5d
kenorb <kenorb@gmail.com>
2013-07-17 18:20:09 +0100

    added cups-pdf for printing into PDF file

--- conf/puppet/Linux/includes/apps.dev.pp
+++ conf/puppet/Linux/includes/apps.dev.pp
@@ -6,19 +6,25 @@
 #
 # COMMON TOOLS
 #
-# Commonly used command lines
+
+# Admin and shell tools
 package { 'sudo': }
 package { 'bash': }
 package { 'realpath': }
-# Commonly used tools
 package { 'screen': }
+
+# File managements
 package { 'mc': }
 # For: sfill - secure free disk and inode space wiper
 package { 'secure-delete': }
+
 # Web tools
 package { 'wget': }
 package { 'links': }
 package { 'lynx': }
+
 # Media tools
 package { 'youtube-dl': }
 
+# Print tools
+package { 'cups-pdf': }
