3d905de5038ea3d9a6c2d71d3c31770decc1e8ce
kenorb <kenorb@users.noreply.github.com>
2016-03-21 15:51:29 +0000

    Better code syntax for drushrc.php.

--- .drush/drushrc.php
+++ .drush/drushrc.php
@@ -3,12 +3,12 @@
 // This script tells drush to include any alias files it can find in git repositories that have a .drush or drush folder.
 // See: http://www.astonishdesign.com/blog/drush-aliases-what-why-and-how
 
-if (exec('git rev-parse --show-toplevel 2> /dev/null', $git)):
+if (exec('git rev-parse --show-toplevel 2> /dev/null', $git)) {
   $r = array_shift($git);
   $options['config'] = array("$r/.drush/drushrc.php", "$r/drush/drushrc.php");
   $options['include'] = array("$r/.drush/commands", "$r/drush/commands");
   $options['alias-path'] = array("$r/.drush", "$r/drush");
-endif;
+}
 
 // Load a drushrc.php configuration file from the current working directory.
 $options['c'] = '.';
